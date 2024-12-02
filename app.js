e2a9c4e8a44b8a9e58dbf59e02ceeb3ba3336d8b
Neeraja858 <meruguneeraja123@gmail.com>
2024-12-02 15:12:13 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import HeroSection from './HeroSection.js';
+
+function App() {
+    return (
+        <div className="App">
+            <HeroSection />
+        </div>
+    );
+}
+
+export default App;
